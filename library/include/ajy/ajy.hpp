e631322dd4a5016ee182c0dfa355202e1d8eb576
ajy-dev <ajy.dev@protonmail.ch>
2026-05-13 14:32:50 +0900

    feat: add umbrella header

---
+++ library/include/ajy/ajy.hpp
@@ -0,0 +1,16 @@
+/**
+ * File: ajy.hpp
+ * Path: library/include/ajy/ajy.hpp
+ * Description: An umbrella header for ajylib.
+ * Author: ajy-dev
+ * Created: 2026-05-13
+ * Updated: Never
+ * Version: 0.1.0
+ */
+
+#ifndef AJY_HPP
+# define AJY_HPP
+
+// Any headers will be included here as they are added.
+
+#endif
