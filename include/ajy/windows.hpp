ba0d23080d9107d4441c7e9872670d6be1aaded2
ajy-dev <ajy.dev@protonmail.ch>
2026-07-02 17:49:44 +0900

    feat: add Windows API umbrella header

---
+++ include/ajy/windows.hpp
@@ -0,0 +1,27 @@
+/**
+ * File: windows.hpp
+ * Path: ajylib/include/ajy/windows.hpp
+ * Description:
+ *	A centralized Windows API include header.
+ * Author: ajy-dev
+ * Created: 2026-07-01
+ * Updated: Never
+ * Version: 0.1.0
+ */
+
+#ifndef AJY_WINDOWS_HPP
+#define AJY_WINDOWS_HPP
+
+#ifndef WIN32_LEAN_AND_MEAN
+# define WIN32_LEAN_AND_MEAN
+#endif
+
+#ifndef NOMINMAX
+# define NOMINMAX
+#endif
+
+#include <Windows.h>
+#include <Winsock2.h>
+#include <WS2tcpip.h>
+
+#endif
