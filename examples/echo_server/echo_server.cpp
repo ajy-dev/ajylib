10817e7b413021ff046bd6b86c1bb817a8a18d58
ajy-dev <ajy.dev@protonmail.ch>
2026-07-04 19:19:14 +0900

    feat: send welcome packet on join, echo via deserialize/serialize roundtrip in echo_server

--- examples/echo_server/echo_server.cpp
+++ examples/echo_server/echo_server.cpp
@@ -13,6 +13,10 @@
 
 #include <ajy/utility/logger.hpp>
 
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
 bool EchoServer::on_connection_request(const char *ip, std::uint16_t port)
 {
 	this->logger->log(ajy::utility::Logger::LogLevel::Info, "connection request from %s:%u", ip, static_cast<unsigned int>(port));
@@ -21,7 +25,13 @@ bool EchoServer::on_connection_request(const char *ip, std::uint16_t port)
 
 void EchoServer::on_client_join(SessionID id) noexcept
 {
+	constexpr std::int64_t WELCOME_MAGIC = 0x7fffffffffffffff;
+	Packet welcome(sizeof(WELCOME_MAGIC));
+
 	this->logger->log(ajy::utility::Logger::LogLevel::Info, "client joined. id: %llu", id);
+
+	welcome << WELCOME_MAGIC;
+	this->send_packet(id, &welcome);
 }
 
 void EchoServer::on_client_leave(SessionID id) noexcept
@@ -31,7 +41,38 @@ void EchoServer::on_client_leave(SessionID id) noexcept
 
 void EchoServer::on_recv(SessionID id, Packet *packet) noexcept
 {
-	this->send_packet(id, packet);
+	std::vector<std::byte> payload;
+	std::size_t payload_size;
+
+	payload_size = packet->get_data_size();
+
+	try
+	{
+		payload.resize(payload_size);
+	}
+	catch (const std::bad_alloc &error)
+	{
+		this->logger->log(ajy::utility::Logger::LogLevel::Fatal, "std::vector::resize(payload): %s", error.what());
+		std::terminate();
+	}
+
+	if (!packet->deserialize(payload.data(), payload_size))
+	{
+		this->logger->log(ajy::utility::Logger::LogLevel::Error, "on_recv(): deserialize failed. id: %llu", id);
+		return;
+	}
+	else
+	{
+		Packet reply(payload_size);
+
+		if (!reply.serialize(payload.data(), payload_size))
+		{
+			this->logger->log(ajy::utility::Logger::LogLevel::Error, "on_recv(): serialize failed. id: %llu", id);
+			return;
+		}
+
+		this->send_packet(id, &reply);
+	}
 }
 
 void EchoServer::on_send(SessionID id, std::size_t size) noexcept
